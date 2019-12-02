c5af627ca76cb8bc2dabcbd543cc7c1fcc8c4455
thomas-ast <56341502+thomas-ast@users.noreply.github.com>
2019-12-02 16:37:36 +0100

    Implémentations des arguments

--- parseur.py
+++ parseur.py
@@ -4,35 +4,65 @@ import subprocess
 import os
 from shutil import rmtree
 from os.path import basename, splitext
+import argparse
+
+# COMMAND LINE ARGUMENTS AND OPTIONS
+parser = argparse.ArgumentParser(description='Convert a pdf file to txt (or xml)', usage='python3 parseur.py [-x|-t] <pdf_to_convert>')
+parser.add_argument('-x', '--xml',
+					help='output to a xml file', 
+					action="store_true", 
+					default=False)
+parser.add_argument('-t', '--txt',
+					help='output to a txt file', 
+					action="store_true", 
+					default=False)
+parser.add_argument(help='pdf file to convert',
+					dest='pdf_to_convert')
+
+args = parser.parse_args()
+
+if(args.xml == False and args.txt == False):
+	print("Please choose between -x and -t")	
+	sys.exit()
+elif(args.xml == True and args.txt == True):
+	print("Please choose only one option between -x and -t")
+	sys.exit()
+
+# args.xml = true/false
+# args.txt = true/false
+# args.pdf_to_convert = pdf a converti
+
 
 # FONCTIONS
 
-def recupererParagrapheDuMot(lines, words, sortie):
+def recupererParagrapheDuMot(lines, words, skipnum=1):
+	sortie = ""
 	for line in range(0,len(lines)):
 		for word in words:
 			if lines[line].lower().find(word.lower()) != -1:
 				while(lines[line].rstrip() != ""):
 					sortie += lines[line+1].rstrip()
 					line = line+1
-					if(re.search("^\d",lines[line+1])):
+					if(not skipnum and re.search("^\d",lines[line+1])):
 						break
-				return sortie;
+				return sortie
 	return "";
 
 def concatXML(var, text):
-	print("\t<"+text+">"+var+"\t</"+text+">")
+	var = var.replace("&","&amp;")
+	print("\t<"+text+">"+var+"</"+text+">")
 
 # CHECK FICHIER
 
-if not sys.argv[1].endswith(".pdf"):
+if not args.pdf_to_convert.endswith(".pdf"):
 	print("Not a pdf file...")
 	sys.exit()
 else:
-	subprocess.run(["pdftotext", sys.argv[1]])
-
-file_to_open = sys.argv[1].replace(".pdf", ".txt")
+	subprocess.run(["pdftotext", args.pdf_to_convert])
 
 # LECTURE DU FICHIER
+
+file_to_open = args.pdf_to_convert.replace(".pdf", ".txt")
 
 f = open(file_to_open, "r")
 lines = f.readlines()
@@ -45,57 +75,45 @@ except:
 	rmtree("parseur_sortie")
 	os.mkdir("parseur_sortie")
 
-# ==================
-original = ""
-titre = ""
-abstract = ""
-# ==================
-
 # NOM FICHIER
 
-original = sys.argv[1]
+original = args.pdf_to_convert
 
 # TITRE
 
-# ========================================
-# TODO: Découper l'année et l'auteur
-# ========================================
-
-titre = sys.argv[1].split("_")[2].replace('.pdf','')
+splitted = args.pdf_to_convert.split("_")
+titre = ""
+auteur = ""
+if(len(splitted) == 3):
+	titre = splitted[2].replace('.pdf','').rstrip()
+	auteur = splitted[0].replace('.pdf','').rstrip()
+else:
+	titre = args.pdf_to_convert
 
 # RECHERCHE ET IMPRESSION
 
-abstract = recupererParagrapheDuMot(lines, ["ABSTRACT"], abstract);
-
-# ========================================
-# TODO: Trouver le reste des sections
-# ========================================
+abstract = recupererParagrapheDuMot(lines, ["Abstract"]);
+references = recupererParagrapheDuMot(lines, ["References"],1);
 
 
 # PRINT TXT
 
-if(len(sys.argv) == 3):
-	if(sys.argv[2] == "-x"):
-		sys.stdout = open('parseur_sortie/' + sys.argv[1].replace('.pdf','.xml'), 'w')
-		print("<article>")
-		concatXML(original,"original")
-		concatXML(titre,"titre")
-		concatXML(abstract,"abstract")
-		print("</article>")
-	if(sys.argv[2] == "-t"):
-		sys.stdout = open('parseur_sortie/' + sys.argv[1].replace('.pdf','.txt'), 'w')
-		print("[ORIGINAL]")
-		print("\t" + original + "\n")
-		print("[TITRE]")
-		print("\t" + titre + "\n")
-		print("[ABSTRACT]")
-		print("\t" + abstract + "\n")
-else:
-	print("Utilisation: python3 parseur.py -x/-t")
-	print("\t-x pour sortir en xml")
-	print("\t-t pour sortir en text avec balises")
-
-
-# FIN
+if(args.xml == True):
+	sys.stdout = open('parseur_sortie/' + args.pdf_to_convert.replace('.pdf','.xml'), 'w')
+	print("<article>")
+	concatXML(original,"preamble")
+	concatXML(titre,"titre")
+	concatXML(auteur,"auteur")
+	concatXML(abstract,"abstract")
+	concatXML(references,"biblio")
+	print("</article>")
+elif(args.txt == True):
+	sys.stdout = open('parseur_sortie/' + args.pdf_to_convert.replace('.pdf','.txt'), 'w')
+	print("[PREAMBLE]")
+	print("\t" + original + "\n")
+	print("[TITRE]")
+	print("\t" + titre + "\n")
+	print("[ABSTRACT]")
+	print("\t" + abstract + "\n")
 
 f.close()
